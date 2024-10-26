c745d07554cf5696807b374696ff7677775db493
Anjudimpy <anjukr55555@gmail.com>
2024-10-26 20:05:34 +0530

    filter songs

---
+++ reduceContact.js
@@ -0,0 +1,15 @@
+//Create a function with name mapContactsToCompanies that accepts an array of contacts and returns an object with the company names as the keys and an array of the corresponding contact names as the values.
+const contacts = [
+    { name: "Alice", company: "ABC Inc." },
+    { name: "Bob", company: "XYZ Corp." },
+    { name: "Charlie", company: "ABC Inc." }
+  ];
+
+  function mapContactsToCompanies(contacts){
+
+    return contacts.reduce((obj, contacts) => {
+
+    },{})
+
+    }
+  
