06f3bebeba75b9c8d17eef868d11aed74cfe6f81
Anjudimpy <anjukr55555@gmail.com>
2024-11-03 12:58:19 +0530

    emailsend

--- NodeJs/emailSend.js
+++ NodeJs/emailSend.js
@@ -1,12 +1,21 @@
 // 1. import nodemailer
 const nodemailer = require('nodemailer');
+const readline = require('readline');
+
+const interface = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout,
+})
 
 // 2. Configure email and send it.
 
 async function sendMail(){
 
+    interface.question("Enter the recipient's email address: ", (recipientEmail) =>{
+ 
 // 1. Create an email transporter.
 // SMTP (Simple Mail Transfer Protocol)
+
 const transporter = nodemailer.createTransport({
     service:'gmail',
     auth:{
@@ -15,21 +24,28 @@ const transporter = nodemailer.createTransport({
     }
 });
 
+       
+
+
 //2. Configure email content
 const mailOptions = {
     from: 'codingninjas2k16@gmail.com',
-    to: 'anjukr55555@gmail.com',
+    to: recipientEmail,
     subject: 'Welcome to NodeJS App',
     text: 'This is an email using nodemailer in NodeJS',
 };
 
 // 3. Send the email
-try{
-    const result = await transporter.sendMail(mailOptions);
-    console.log("Email sent successfully");
-}catch(err){
-    console.log('Email send failer with error: '+ err);
-}
+transporter.sendMail(mailOptions, (err, info) => {
+    if (err) {
+        console.log('Email send failed with error:', err);
+    } else {
+        console.log('Email sent successfully:', info.response);
+    }
+    // Close readline interface
+    rl.close();
+});
+});
 }
 
 sendMail();
