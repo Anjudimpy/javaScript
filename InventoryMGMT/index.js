0cc48cd7830b5743a7da2b44d4f1277fe54947ee
Anjudimpy <anjukr55555@gmail.com>
2024-11-09 14:27:54 +0530

    ejs layout

--- InventoryMGMT/index.js
+++ InventoryMGMT/index.js
@@ -1,12 +1,20 @@
-import express from 'express';
-import ProductController from './src/controllers/product.controllers.js'
+import express from 'express'
+import ProductController from './src/controllers/product.controller.js';
+import ejsLayouts from 'express-ejs-layouts'
+import path from 'path';
 
-const server = express()
+const server = express();
 
-//create an instance of ProductController
-const productController = new ProductController()
-server.get('/', productController.getProducts)
+// setup view engine settings
+server.set("view engine", "ejs");
+// path of our views
+server.set("views", path.join(path.resolve(),"src",'views')); 
 
-server.use(express.static('src/views'))
+server.use(ejsLayouts)
 
-server.listen(3400)
+// create an instance of ProductController
+const productController = new ProductController(); 
+server.get('/', (productController.getProducts));
+
+server.use(express.static('src/views'));
+export default server
---
+++ InventoryMGMT/server.js
@@ -0,0 +1,4 @@
+import server from "./index.js";
+server.listen(3400, () => {
+  console.log("server is listening on 3400");
+});
