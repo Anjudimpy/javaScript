e372bf30cf43bb9a0ba26f45bb2741a2a432334f
Anjudimpy <anjukr55555@gmail.com>
2024-11-13 13:21:02 +0530

    Regigtration and login page

--- InventoryMGMT/index.js
+++ InventoryMGMT/index.js
@@ -1,4 +1,5 @@
 import express from 'express'
+import UserController from './src/controllers/user.controller.js';
 import ProductController from './src/controllers/product.controller.js';
 import ejsLayouts from 'express-ejs-layouts'
 import path from 'path';
@@ -6,7 +7,6 @@ import validateRequest from './src/middlewares/validation.middleware.js';
 import { uploadFile } from './src/middlewares/file-upload.middlewere.js';
 
 const server = express();
-
 server.use(express.static('public'));
 
 // parse from data
@@ -18,9 +18,16 @@ server.set("views", path.join(path.resolve(),"src",'views'));
 
 server.use(ejsLayouts)
 
-// create an instance of ProductController
+// create an instance of Controller
 const productController = new ProductController(); 
-server.get('/', (productController.getProducts));
+const userController = new UserController();
+
+server.get('/register', userController.getRegister);
+server.get('/login', userController.getLogin);
+server.post('/login',userController.postLogin);
+server.post('/register', userController.postRegister);
+
+server.get('/', productController.getProducts);
 server.get('/new', productController.getAddForm);
 server.post('/', uploadFile.single('imageUrl'), validateRequest, productController.addNewProduct);
 server.get('/update-product/:id', productController.getUpdateProductView);
---
+++ InventoryMGMT/src/controllers/user.controller.js
@@ -0,0 +1,33 @@
+import ProductModel from "../models/product.model.js";
+import UserModel from "../models/user.model.js";
+
+export default class UserController {
+    getRegister(req, res) {
+        res.render('register');
+    }
+
+    getLogin(req, res) {
+        res.render('login', {errorMessage: null});
+    }
+
+    postRegister(req, res) {
+        const {name, email, password} = req.body;
+        UserModel.add(name, email, password);
+        res.render('login',{errorMessage: null});
+    }
+
+    postLogin(req, res) {
+        const {email, password} = req.body;
+        const user = UserModel.isValidUser(
+            email,
+            password
+        );
+        if(!user){
+           return  res.render('login',{
+                errorMessage:'Invalid Credentials'
+            });
+        }
+        var products = ProductModel.get();
+        res.render('products', {products}); 
+    }
+}
