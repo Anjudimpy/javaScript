8b2a281ce5f21ccea24c8c138042a599031b5bec
Anjudimpy <anjukr55555@gmail.com>
2024-11-29 16:06:25 +0530

    Encapsulation

---
+++ JavaScript/OOPS/encapsulation.js
@@ -0,0 +1,29 @@
+class VehicleCl {
+    //Private properties
+    #regNumber;
+    //constructor
+    constructor(name, color, wheels, number){
+        
+       this.name = name;
+       this.color = color;
+       this.wheels = wheels;
+       this.#regNumber = number;
+    }
+ //Prvate method
+
+ #getRegNumber(number){
+    return number;
+}
+    //methods
+
+    getDetails() {
+        console.log(`The ${this.name} is ${this.color} in color
+            It has ${this.wheels} wheels and Registration Number ${this.#getRegNumber(this.#regNumber)}`)
+    }
+
+   
+
+}
+
+const car1 = new VehicleCl("Audi",'red', 4,45556);
+car1.getDetails();
