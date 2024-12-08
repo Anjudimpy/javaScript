a950406bd652df40ddec538ac235e47a8a11f13e
Anjudimpy <anjukr55555@gmail.com>
2024-12-08 19:03:42 +0530

    Promise

---
+++ JavaScript/DOM Manipulation/promise.js
@@ -0,0 +1,13 @@
+const request = new Promise((resolve, reject) => {
+    setTimeout(() => {
+        //console.log('Promise initiated');
+        resolve({firstName: 'Falak', lastName: 'Chandni'});
+        //reject('Request has been rejected!!');
+    },1000);
+});
+
+request.then((value) => {
+    console.log(value);
+})
+.catch((err) => console.log(err))
+.finally(() => console.log('request completed'));
