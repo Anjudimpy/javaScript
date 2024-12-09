814d09e63630a56ab620efb937dd093b5484202c
Anjudimpy <anjukr55555@gmail.com>
2024-12-09 13:03:41 +0530

    Error Handling

--- JavaScript/DOM Manipulation/fetchFunction.js
+++ JavaScript/DOM Manipulation/fetchFunction.js
@@ -9,9 +9,16 @@ function getDetails(id){
 
     fetch(`https://dummyjson.com/users/${id}`)
     .then((response) => {
+        if(!response.ok){
+            throw new Error("Id does not match any data")
+        }
         return response.json();
     })
-    .then((user) => console.log(user));
+    .then((user) => console.log(user))
+    //handle error
+    .catch((err)=> {
+       console.error(err);
+    });
 }
 
-getDetails(2);
+getDetails(1);
